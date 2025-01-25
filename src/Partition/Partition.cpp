d5eec31af28bfc0e42d53834bdc21a3b2a06c357
1-Nishanth-1 <nishanthrn1@gmail.com>
2025-01-25 16:38:23 +0530

    fix: nvme support added

--- src/Partition/Partition.cpp
+++ src/Partition/Partition.cpp
@@ -4,11 +4,13 @@
 #include <blkid/blkid.h>
 #include "Partition.h"
 
-int DisplayPartitions(const char* path) {
+int DisplayPartitions(const char *path)
+{
    blkid_partlist list;
    blkid_probe pr = blkid_new_probe_from_filename(path);
    list = blkid_probe_get_partitions(pr);
-   if (!list) {
+   if (!list)
+   {
       err(1, "Failed to open %s", path);
    }
    blkid_partlist ls;
@@ -18,7 +20,8 @@ int DisplayPartitions(const char* path) {
    nparts = blkid_partlist_numof_partitions(ls);
    printf("Number of partitions:%d\n", nparts);
 
-   if (nparts <= 0){
+   if (nparts <= 0)
+   {
       printf("Please enter correct device name! e.g. \"/dev/sdc\"\n");
       return 1;
    }
@@ -27,27 +30,48 @@ int DisplayPartitions(const char* path) {
    const char *label;
    const char *type;
 
+   if (strncmp(path, "dev/nvme", 8) == 0)
+   {
+      for (i = 0; i < nparts; i++)
+      {
+         char dev_name[20];
 
-   for (i = 0; i < nparts; i++) {
-      char dev_name[20];
+         sprintf(dev_name, "%sp%d", path, (i + 1));
 
-      sprintf(dev_name, "%s%d", path, (i+1));
+         pr = blkid_new_probe_from_filename(dev_name);
+         blkid_do_probe(pr);
 
-      pr = blkid_new_probe_from_filename(dev_name);
-      blkid_do_probe(pr);
+         blkid_probe_lookup_value(pr, "UUID", &uuid, NULL);
 
-      blkid_probe_lookup_value(pr, "UUID", &uuid, NULL);
+         blkid_probe_lookup_value(pr, "LABEL", &label, NULL);
 
-      blkid_probe_lookup_value(pr, "LABEL", &label, NULL);
+         blkid_probe_lookup_value(pr, "TYPE", &type, NULL);
 
-      blkid_probe_lookup_value(pr, "TYPE", &type, NULL);
+         printf("Name=%s, UUID=%s, LABEL=%s, TYPE=%s\n", dev_name, uuid, label, type);
+      }
+   }
+   else
+   {
+      for (i = 0; i < nparts; i++)
+      {
+         char dev_name[20];
 
-      printf("Name=%s, UUID=%s, LABEL=%s, TYPE=%s\n", dev_name, uuid, label, type);
+         sprintf(dev_name, "%s%d", path, (i + 1));
 
+         pr = blkid_new_probe_from_filename(dev_name);
+         blkid_do_probe(pr);
+
+         blkid_probe_lookup_value(pr, "UUID", &uuid, NULL);
+
+         blkid_probe_lookup_value(pr, "LABEL", &label, NULL);
+
+         blkid_probe_lookup_value(pr, "TYPE", &type, NULL);
+
+         printf("Name=%s, UUID=%s, LABEL=%s, TYPE=%s\n", dev_name, uuid, label, type);
+      }
    }
 
    blkid_free_probe(pr);
 
    return 0;
 }
-
